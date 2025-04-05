164094309d80ddccc6e2915100dc3622f501a2cd
Ethan Gould <ethangould@rocketmail.com>
2025-04-05 11:54:59 +0100

    Created python file with given functions

---
+++ ping_proxy.py
@@ -0,0 +1,45 @@
+import requests
+import sys
+import json
+
+def create_team(team_name: str, team_email: str) -> str:
+    r = requests.post(
+        "https://api.scrapemequickly.com/register",
+        data=json.dumps({"team_name": team_name, "team_email": team_email}),
+        headers={"Content-Type": "application/json"}
+    )
+
+    if r.status_code != 200:
+        print(r.json())
+        print("Failed to create a team")
+        sys.exit(1)
+
+    return r.json()["data"]["team_id"]
+
+
+def start_scraping_run(team_id: str) -> str:
+    r = requests.post(f"https://api.scrapemequickly.com/scraping-run?team_id={team_id}")
+
+    if r.status_code != 200:
+        print(r.json())
+        print("Failed to start scraping run")
+        sys.exit(1)
+
+    return r.json()["data"]["scraping_run_id"]
+
+def submit(answers: dict, scraping_run_id: str) -> bool:
+    r = requests.post(
+        f"https://api.scrapemequickly.com/cars/solve?scraping_run_id={scraping_run_id}",
+        data=json.dumps(answers),
+        headers={"Content-Type": "application/json"}
+    )
+
+    if r.status_code != 200:
+        print(r.json())
+        print("Failed to submit answers")
+        return False
+
+    return True
+
+start_scraping_run("cc675f0b-1205-11f0-8f44-0242ac120003")
+print("test")
